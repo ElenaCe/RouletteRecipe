84b37f1a5ccedf217c7e4f380ed39b5c9f3c721d
Elena <elenacellitti92@gmail.com>
2019-06-24 15:05:12 +0100

    Summary
    - Fixed links in nac bar to different html pages
    - removed formpage.py as content is in ingpage.py

--- ingpage.py
+++ ingpage.py
@@ -10,10 +10,53 @@ def open_page():
     #render_template() to tell Flask what HTML page to display
     return render_template("index.html")
 
+@app.route("/formpage", methods=["GET"])
+def open_formpage():
+    return render_template("formpage.html")
+
+@app.route("/aboutus", methods=["GET"])
+def open_aboutus():
+    return render_template("aboutus.html")
+
+@app.route("/formpage", methods=["POST"])
+def submitrecipe():
+    form_data = request.form
+    email = form_data["email"]
+    name = form_data["name"]
+    lname = form_data["lname"]
+    recipetitle = form_data["recipetitle"]
+    ingredients = form_data["ingredients"]
+    recipe = form_data["recipe"]
+    fileToUpload = request.files["fileToUpload"]
+    fullname= name + " " + lname
+    
+    print(fileToUpload)
+
+    #finds the dotenv file and sets it off 
+    load_dotenv()
+    #assign variables, asks the to get the mailgun_api and call it api_key
+    api_key = os.getenv("MAILGUN_API_KEY")
+    api_domain = os.getenv("MAILGUN_DOMAIN")
+
+    #post - sends info to the server
+    response = requests.post(
+            "https://api.mailgun.net/v3/{}/messages".format(api_domain),
+            auth=("api", api_key),
+            #in data we're def different variables from, to, subj,..
+            #files=[("attachment", (namefile.jpg, fileToUpload))],
+            files=[("attachment", (fileToUpload.filename, fileToUpload))],
+            data={"from": "Recipe Roulette team <mailgun@{}>".format(api_domain),
+                "to": email,
+                "subject": "{}! Your submitted recipe for Recipe Roulette".format(name),
+                "text": "Hi {} \n \n Thank you! We have successfully received your recipe! \n \n {} \n \n Ingredients: \n {} \n \n Recipe: \n {} \n \n Thank you from the Recipe Roulette team".format(name,recipetitle,ingredients,recipe)})
+    print(response.json())
+    thankyoumsg= " Thank you " + fullname.title() + " for sending your recipe! "
+    return render_template('formpage.html', value=thankyoumsg)
+    
+
+
 @app.route("/search", methods=["POST"])
 def submiting():
-
-
     ing = request.form['ing']
 
     load_dotenv()
@@ -46,3 +89,4 @@ def submiting():
 
     return render_template('./recipe.html',recurl=rec,rectitle=rectitle,recimage=recimage,recurl1=rec1,rectitle1=rectitle1,recimage1=recimage1,recurl2=rec2,rectitle2=rectitle2,recimage2=recimage2)
 
+app.run(debug=True) 
